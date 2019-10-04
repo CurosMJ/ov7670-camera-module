d15d081084e2ee152e1c5eda7ef357fc31b8bd8c
Manan Jadhav <curosmj@gmail.com>
2019-10-04 11:44:39 -0400

    improve API style

--- ov7670_camera.cpp
+++ ov7670_camera.cpp
@@ -9,6 +9,7 @@
 #include <util/twi.h>
 #include <util/delay.h>
 #include <avr/pgmspace.h>
+#include <ov7670_camera.h>
 
 #define F_CPU 16000000UL
 #define vga   0
@@ -19,6 +20,8 @@
 #define bayerRGB  2
 #define camAddr_WR  0x42
 #define camAddr_RD  0x43
+
+//#define PIN_D(x) (1 << x)
 
 /* Registers */
 #define REG_GAIN    0x00  /* Gain lower 8 bits (rest in vref) */
@@ -534,33 +537,6 @@ void camInit(void){
   wrReg(REG_COM10, 32);//PCLK does not toggle on HBLANK.
 }
 
-// void arduinoUnoInut(void) {
-//   cli();//disable interrupts
-  
-//     /* Setup the 8mhz PWM clock
-//   * This will be on pin 11*/
-//   DDRB |= (1 << 3);//pin 11
-//   ASSR &= ~(_BV(EXCLK) | _BV(AS2));
-//   TCCR2A = (1 << COM2A0) | (1 << WGM21) | (1 << WGM20);
-//   TCCR2B = (1 << WGM22) | (1 << CS20);
-//   OCR2A = 0;//(F_CPU)/(2*(X+1))
-//   DDRC &= ~15;//low d0-d3 camera // 00001111
-//   DDRD &= ~252;//d7-d4 and interrupt pins // 11111100
-//   _delay_ms(3000);
-  
-//     //set up twi for 100khz
-//   TWSR &= ~3;//disable prescaler for TWI
-//   TWBR = 72;//set to 100khz
-  
-//     //enable serial
-//   UBRR0H = 0;
-//   UBRR0L = 1;//0 = 2M baud rate. 1 = 1M baud. 3 = 0.5M. 7 = 250k 207 is 9600 baud rate.
-//   UCSR0A |= 2;//double speed aysnc
-//   UCSR0B = (1 << RXEN0) | (1 << TXEN0);//Enable receiver and transmitter
-//   UCSR0C = 6;//async 1 stop bit 8bit char no parity bits
-// }
-
-
 void StringPgm(const char * str){
   do{
       while (!(UCSR0A & (1 << UDRE0)));//wait for byte to transmit
@@ -571,8 +547,6 @@ void StringPgm(const char * str){
 
 static void captureImg(uint16_t wg, uint16_t hg){
   uint16_t y, x;
-
-  //StringPgm(PSTR("*RDY*"));
 
   while (!(PIND & 8));//wait for high
   while ((PIND & 8));//wait for low
@@ -594,26 +568,28 @@ static void captureImg(uint16_t wg, uint16_t hg){
     _delay_ms(100);
 }
 
-void capture_and_transmit_image() {
-  captureImg(320, 240);
-}
-
-void setup_camera() {
+void Camera::setup_camera() {
   camInit();
   setRes();
   setColor();
   wrReg(0x11, 10);
 }
 
-// void setup(){
-//   arduinoUnoInut();
-//   camInit();
-//   setRes();
-//   setColor();
-//   wrReg(0x11, 10); //Earlier it had the value: wrReg(0x11, 12); New version works better for me :) !!!!
-// }
+void Camera::setup_arduino() {
+  DDRB |= (1 << 3); //pin 11
+  ASSR &= ~(_BV(EXCLK) | _BV(AS2));
+  TCCR2A = (1 << COM2A0) | (1 << WGM21) | (1 << WGM20);
+  TCCR2B = (1 << WGM22) | (1 << CS20);
+  OCR2A = 0;//(F_CPU)/(2*(X+1))
+  DDRC &= ~15;//low d0-d3 camera // 00001111
+  DDRD &= ~252;//d7-d4 and interrupt pins // 11111100
+  _delay_ms(3000);
 
+    //set up twi for 100khz
+  TWSR &= ~3;//disable prescaler for TWI
+  TWBR = 72;//set to 100khz
+}
 
-// void loop(){
-//   captureImg(320, 240);
-// }
+void Camera::capture_and_transmit(unsigned short width, unsigned short height) {
+  captureImg(width, height);
+}
--- ov7670_camera.h
+++ ov7670_camera.h
@@ -1,7 +1,11 @@
 #ifndef ov7670_camera_h
 #define ov7670_camera_h
 
-void setup_camera();
-void capture_and_transmit_image();
+class Camera {
+  public:
+    void setup_arduino();
+    void setup_camera();
+    void capture_and_transmit(unsigned short, unsigned short);
+};
 
 #endif
